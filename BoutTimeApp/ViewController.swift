ab03b417bb1d833cd9831237325153ef0fd48310
Lorenzo Gonzalez <wlorenzogonzalez@Lorenzos-MacBook-Pro.local>
2017-05-04 20:00:59 +1000

    second view controller needs to be enabled and aspects made functional

--- BoutTimeApp/ViewController.swift
+++ BoutTimeApp/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var threeLabel: UILabel!
     @IBOutlet weak var fourLabel: UILabel!
     
+    @IBOutlet weak var gameOverCheckScoreButton: UIButton!
+    
     @IBOutlet weak var countdownLabel: UILabel!
     
     func assignEventsToLabels(){
@@ -21,6 +23,8 @@ class ViewController: UIViewController {
         twoLabel.text = orderOfEvents[generateRandomEvent()]
         threeLabel.text = orderOfEvents[generateRandomEvent()]
         fourLabel.text = orderOfEvents[generateRandomEvent()]
+        
+        shakeToCheckAnswerButtonAsLabel.isHidden = false
         
         usedIndex = []
     }//End assignEventsToLabel
@@ -36,14 +40,22 @@ class ViewController: UIViewController {
     
     func startCountdown(){
         if counter != 0 {
-            countdownLabel.text = ":\(counter)"
+            countdownLabel.text = ": \(counter)"
             counter -= 1
         } else {
-            timer.invalidate()
-            countdownLabel.text = "You Lose!"
-            counter = 59
-        }
+            
+            do{
+                try isCorrectOrIncorrect()
+            } catch {}// FIXME: Complete do/catch
+            
+        }//End if counter
     }//End startCountdown
+    
+    // MARK: Keeping Score
+    
+    var correctAnswers = 0
+    var incorrectAnswers = 0
+    
     
     
     // MARK: viewDidLoad
@@ -52,6 +64,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         assignEventsToLabels()
+        shakeToCheckAnswerButtonAsLabel.isEnabled = false
+        gameOverCheckScoreButton.isHidden = true
         countdownTimer()
     }
     
@@ -105,5 +119,88 @@ class ViewController: UIViewController {
         threeLabel.text = fourLabelText
     }
     
-}
+    
+    // MARK: Checking Answers
+    
+    enum NoText: Error{
+        case noText
+    }
+    
+    
+    @IBOutlet weak var shakeToCheckAnswerButtonAsLabel: UIButton!
+    @IBAction func shakeToCheckAnswerButton(_ sender: UIButton) {
+        shakeToCheckAnswerButtonAsLabel.setTitle("Shake to check answer!", for: UIControlState.normal)
+        shakeToCheckAnswerButtonAsLabel.isEnabled = false
+        shakeLabel.text = "1:00"
+        assignEventsToLabels()
+        countdownTimer()
+    }
+    
+    func isCorrectOrIncorrect() throws {
+        
+            
+            guard let firstSpot = oneLabel.text, let secondSpot = twoLabel.text, let thirdSpot = threeLabel.text, let fourthSpot = fourLabel.text else {
+                throw NoText.noText
+            }//End guard
+            
+            let userDetermiendEventOrder = [firstSpot, secondSpot, thirdSpot, fourthSpot]
+            
+            if userDetermiendEventOrder == orderOfEvents {
+                shakeLabel.text = "Correct!"
+                correctAnswers += 1
+            } else {
+                shakeLabel.text = "Incorrect!"
+                incorrectAnswers += 1
+            }//End if
+        
+            shakeToCheckAnswerButtonAsLabel.isEnabled = true
+            shakeToCheckAnswerButtonAsLabel.setTitle("NEXT ->", for: UIControlState.normal)// FIXME: shakeToCheckAnswer make unhidden when start next round
+            timer.invalidate()
+            counter = 59
+        
+        
+        if isRoundOver() == true{
+            //present new screen displaying score
+        }//End if
+            
+        
+    }//End isCorrectOrIncorrect
+    
+    
+    
+    @IBOutlet weak var shakeLabel: UILabel!
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?){
+        if motion == .motionShake {
+            do {
+                try isCorrectOrIncorrect()
+            } catch {}// FIXME: Complete do/ catch
+        }//End If gesture
+    }
+    
+    func isRoundOver() -> Bool{
+        if correctAnswers + incorrectAnswers == 6 {
+            shakeToCheckAnswerButtonAsLabel.isHidden = true
+            gameOverCheckScoreButton.isHidden = false
+            
+            // FIXME: Play again button must reset correct and incorrect answers to 0
+            
+            return true
+        } else {
+            return false
+        }//End if
+    }//End isRoundOver
+    
+    
+    
+    
+}//End ViewController
+
+
+
+
+
+
+
+
 
